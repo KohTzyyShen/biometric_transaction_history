7c9e3441d5398260e7a3f0766aa5276c03580df8
KohTzyyShen <kohtzyyshen@gmail.com>
2025-05-16 13:06:01 +0800

    home screen feature (hardcode)

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { SafeAreaView, View, Text, Button } from 'react-native';
 
@@ -14,3 +15,49 @@ export default function HomeScreen({ navigation }: any) {
     </SafeAreaView>
   );
 }
+*/
+
+import React from 'react';
+import { SafeAreaView, View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }: any) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topColumn}>
+        <Text style={styles.text1}>Hi Koh Tzyy Shen</Text>
+        <Text style={styles.text2}>How can I help you today</Text>
+      </View>
+
+      <View style={styles.centerButton}>
+        <Button
+          title="Show my transaction"
+          onPress={() => navigation.navigate('Transaction History')}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topColumn: {
+    marginTop: 90,
+    alignItems: 'center',
+  },
+  text1: {
+    color: '#0000e6',
+    fontSize: 18,
+    marginBottom: 13,
+  },
+  text2: {
+    color: '#4f4f4f',
+    fontSize: 16,
+  },
+  centerButton: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 40,
+  },
+});
